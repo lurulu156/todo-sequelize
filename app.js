682f52573d99983cba8cbb2e03d3c96528ba4804
lurulu156 <kzzlin9@gmail.com>
2022-12-12 15:44:08 +0800

    feat: add flash message

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 const PORT = 3000
 const passport = require('passport')
 const routes = require('./routes')
+const flash = require('connect-flash')
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -22,12 +23,12 @@ app.use(express.urlencoded({ extended: true })) //body-parser
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  // res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
-  // res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
