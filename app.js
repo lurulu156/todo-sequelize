cebc96277d2b819b0e55be0f08a6ea828d9fe570
lurulu156 <kzzlin9@gmail.com>
2022-12-12 15:34:45 +0800

    feat: add authenticator to todos and home route

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(express.urlencoded({ extended: true })) //body-parser
 app.use(methodOverride('_method'))
 
 usePassport(app)
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  // res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  // res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,16 @@
 const express = require('express')
 const router = express.Router()
 // 準備引入路由模組
-// 引入 home 模組程式碼
+// 引入 模組
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
-// const { authenticator } = require('../middleware/auth') // 掛載 middleware
+const { authenticator } = require('../middleware/auth') // 掛載 middleware
 // const auth = require('./modules/auth')
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-// router.use('/todos', authenticator, todos)
-// router.use('/users', users)
-// router.use('/auth', auth)
-// router.use('/', authenticator, home)
-router.use('/todos', todos)
+// 將網址結構符合字串的 request 導向模組 
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
-router.use('/', home)
+// router.use('/auth', auth)
+router.use('/', authenticator, home)
 // 匯出路由器
 module.exports = router
